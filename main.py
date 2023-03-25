fa884b97d8ad0084d20d518194a8d36a0134183b
Matt Distell <mdistell22@gmail.com>
2023-03-25 22:04:44 +0100

    adding restaurant rating functionality

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request
+from flask_restful import Api, Resource, abort, reqparse
+
 
 
 app = Flask(__name__)
 api = Api(app)
 
 
-names = {"Matt": {"age": 21, "gender": "male"},
-        "Bill" : {"age": 70, "gender": "male"}}
 
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name]
-    
+location_put_args = reqparse.RequestParser()
+location_put_args.add_argument("city", type=str, help="Name of city is required", required=True)
+location_put_args.add_argument("restaurant", type=str, help="Restaurant of city", required=True)
+location_put_args.add_argument("rating", type=int, help="Rating of city", required=True)
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
+restaurants = {}
+
+# names = {"Matt": {"age": 21, "gender": "male"},
+#         "Bill" : {"age": 70, "gender": "male"}}
+
+def abort_if_restaurant_id_not_exist(restaurant_id):
+    if restaurant_id not in restaurants:
+        abort("restaurant_id is not valid. . .")
+
+
+class Travel(Resource):
+    def get(self, restaurant_id):
+        return restaurants[restaurant_id]
+
+    def put(self, restaurant_id):
+        args = location_put_args.parse_args()
+        restaurants[restaurant_id] = args
+        return restaurants[restaurant_id], 201
+
+
+api.add_resource(Travel, "/travel/<int:restaurant_id>")
 
 
 
--- test.py
+++ test.py
@@ -4,6 +4,9 @@ import requests
 BASE = "http://127.0.0.1:5000/"
 
 
-response = requests.get(BASE + "helloworld" + "/Bill")
+response = requests.put(BASE + "travel/1", {"rating": 10, "city": "London", "restaurant": "McDonalds"})
+print(response.json())
+input()
 
+response = requests.get(BASE + "travel/1")
 print(response.json())
